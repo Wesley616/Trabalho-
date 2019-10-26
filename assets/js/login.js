549d6760ca2abbca76d44c0fd1bce86979ebae15
Wesley616 <56004346+Wesley616@users.noreply.github.com>
2019-10-26 10:02:38 -0300

    Trabalho 26/10

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,4 @@ jQuery(function($)
     function supportsPlaceholder() 
     { return 'placeholder' in document.createElement('input') && 'placeholder' in document.createElement('textarea'); }
     if(!supportsPlaceholder()) { $('html').addClass('no-placeholder'); } 
-}); 
+});
